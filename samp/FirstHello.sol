dbac403293281cb6f1b4a6edd100af6f5160a651
Dave <davepeck@gmail.com>
2021-09-22 15:09:52 -0700

    WIP

---
+++ samp/FirstHello.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: who cares?
+pragma solidity ^0.8.0;
+
+contract FirstHello {
+    function sayHello() public pure returns (string memory) {
+        return "Hello from the FirstHello contract!";
+    }
+}
