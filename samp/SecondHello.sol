b68eb14277edc6989ee137d371b5b4f26ad3930b
Dave <davepeck@gmail.com>
2021-09-23 09:21:34 -0700

    Bye bye samp

--- samp/SecondHello.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: who cares?
-pragma solidity ^0.8.0;
-
-contract FirstHello {
-    function sayHello() public pure returns (string memory) {
-        return "Hello from the FirstHello contract!";
-    }
-}
