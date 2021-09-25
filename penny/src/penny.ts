e0b8b7b06dc568a0435720daab7f52a0e3106f7e
Dave <davepeck@gmail.com>
2021-09-24 17:18:34 -0700

    WIP: metamask connection

--- penny/src/penny.ts
+++ penny/src/penny.ts
@@ -1,8 +1,12 @@
-class PennyJarFrontend {
-  run() {
-    console.log("I am running!");
-  }
-}
+const connectButton = document.getElementById("connect") as HTMLButtonElement;
+const donateButton = document.getElementById("donate") as HTMLButtonElement;
+const takeButton = document.getElementById("take") as HTMLButtonElement;
 
-const frontend: PennyJarFrontend = new PennyJarFrontend();
-frontend.run();
+const isMetaMaskInstalled = () => {
+  const { ethereum } = window as { ethereum?: { isMetaMask: boolean } };
+  return Boolean(ethereum?.isMetaMask);
+};
+
+connectButton.innerText = isMetaMaskInstalled()
+  ? "Connect with MetaMask"
+  : "Click here to install MetaMask";
